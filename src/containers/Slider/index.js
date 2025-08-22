4c2fec04bb4c3920e04875d3ebc4e422c34f25ec
Ikerchalene Wiam <imounane.wiam@gmail.com>
2025-08-22 13:07:09 +0200

    correction dans le slider pour enlever la page blanche

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -1,35 +1,36 @@
 import { useEffect, useState } from "react";
 import { useData } from "../../contexts/DataContext";
 import { getMonth } from "../../helpers/Date";
-
 import "./style.scss";
 
 const Slider = () => {
   const { data } = useData();
   const [index, setIndex] = useState(0);
-  const byDateDesc = data?.focus.sort((evtA, evtB) =>
-    new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
-  );
-  const nextCard = () => {
-    setTimeout(
-      () => setIndex(index < byDateDesc.length ? index + 1 : 0),
-      5000
-    );
-  };
+  
+  const byDateDesc = data?.focus?.sort((evtA, evtB) =>
+    new Date(evtB.date) - new Date(evtA.date)
+  ) || [];
+
   useEffect(() => {
-    nextCard();
-  });
+    const timer = setInterval(() => {
+      setIndex(prevIndex => 
+        prevIndex === (byDateDesc.length - 1) ? 0 : prevIndex + 1
+      );
+    }, 5000);
+
+    return () => clearInterval(timer);
+  }, [byDateDesc.length]);
+
   return (
     <div className="SlideCardList">
-      {byDateDesc?.map((event, idx) => (
-        <>
+      {byDateDesc.map((event) => (
+        <div key={event.title}>
           <div
-            key={event.title}
             className={`SlideCard SlideCard--${
-              index === idx ? "display" : "hide"
+              index === byDateDesc.indexOf(event) ? "display" : "hide"
             }`}
           >
-            <img src={event.cover} alt="forum" />
+            <img src={event.cover} alt={event.title} />
             <div className="SlideCard__descriptionContainer">
               <div className="SlideCard__description">
                 <h3>{event.title}</h3>
@@ -40,17 +41,18 @@ const Slider = () => {
           </div>
           <div className="SlideCard__paginationContainer">
             <div className="SlideCard__pagination">
-              {byDateDesc.map((_, radioIdx) => (
+              {byDateDesc.map((dot) => (
                 <input
-                  key={`${event.id}`}
+                  key={dot.title}
                   type="radio"
                   name="radio-button"
-                  checked={idx === radioIdx}
+                  checked={index === byDateDesc.indexOf(dot)}
+                  readOnly
                 />
               ))}
             </div>
           </div>
-        </>
+        </div>
       ))}
     </div>
   );
